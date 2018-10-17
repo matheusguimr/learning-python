0931a828adf88846a85e1c20b280c02afe5c8f16
Matheus Guimaraes <matheusguimr@gmail.com>
2018-10-16 21:57:25 -0300

    Add pylint overview

---
+++ errors_and_exceptions_handling/pylint_overview.py
@@ -0,0 +1,20 @@
+'''
+A Very Simple Script
+'''
+
+
+def myfunc():
+    '''
+    A simple function
+    '''
+
+    first = 1
+    second = 2
+
+    print(first)
+    print(second)
+
+
+myfunc()
+
+# Run: pylint pylint_overview.py
